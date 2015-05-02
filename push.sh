abfc9f2671736cdd213b08d8181ea4f5e7267fac
Shwet Solanki <me@shwet.net>
2015-05-02 11:18:46 +0530

    Conflict with UIAlertView Category resolved

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git add -A
-git commit -m "Minor bug fixes"
+git commit -m "Conflict with UIAlertView Category resolved"
 git push origin master
-git tag -d 2.0.1
-git push origin :refs/tags/2.0.1
-git tag 2.0.1
-git push origin 2.0.1
+git tag -d 2.0.2
+git push origin :refs/tags/2.0.2
+git tag 2.0.2
+git push origin 2.0.2
