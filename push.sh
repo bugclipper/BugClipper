efe6960d67cfabaa09737a7338eccae050d438f4
Shwet Solanki <me@shwet.net>
2015-06-25 14:41:13 +0530

    Floating menu added on new Window

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git add -A
-git commit -m "Conflict with UIAlertView Category resolved"
+git commit -m "Floating menu added on new Window"
 git push origin master
-git tag -d 2.0.2
-git push origin :refs/tags/2.0.2
-git tag 2.0.2
-git push origin 2.0.2
+git tag -d 2.0.3
+git push origin :refs/tags/2.0.3
+git tag 2.0.3
+git push origin 2.0.3
