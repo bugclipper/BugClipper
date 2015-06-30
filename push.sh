7bee51151f49532b1c3c80548df69a92b9396da8
Shwet Solanki <me@shwet.net>
2015-06-30 23:06:49 +0530

    Framework complilation issue resolved

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git add -A
-git commit -m "Floating menu added on new Window"
+git commit -m "Framework complilation issue resolved"
 git push origin master
-git tag -d 2.0.3
-git push origin :refs/tags/2.0.3
-git tag 2.0.3
-git push origin 2.0.3
+git tag -d 2.0.4
+git push origin :refs/tags/2.0.4
+git tag 2.0.4
+git push origin 2.0.4
